677d046e59eba6b7534d6fb6f478528795e3bbb1
Iñigo Biedma <biedma.64567@e.unavarra.es>
2017-03-22 16:48:24 +0100

    Dar aviso si no podemos leer el número de teléfono

--- App/KEDADAS/app/src/main/java/com/none/kedadas/LogIn.java
+++ App/KEDADAS/app/src/main/java/com/none/kedadas/LogIn.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Toast;
 
 public class LogIn extends AppCompatActivity {
 
@@ -20,7 +21,16 @@ public class LogIn extends AppCompatActivity {
         Intent intent = new Intent(this, InitialPage.class);
         startActivity(intent);
 
-        //  String _phone = getPhoneNumber();
+        try {
+            String _phone = getPhoneNumber();
+        } catch (SecurityException e) {
+            Toast toast = Toast.makeText(getApplicationContext(), "No se pudo leer el número de teléfono", Toast.LENGTH_LONG);
+            toast.show();
+            System.out.println("Error!");
+            // close current activity
+            finish();
+        }
+
         //TODO we have to check if we have the pair android id-phone number on ur database
         /*if (_phone.isEmpty()){
 
@@ -36,8 +46,7 @@ public class LogIn extends AppCompatActivity {
     }
 
     //This method is used to take the phone number from the device automatically
-    private String getPhoneNumber(){
-
+    private String getPhoneNumber() throws SecurityException{
         TelephonyManager mTelephonyManager;
         mTelephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         return mTelephonyManager.getLine1Number();
