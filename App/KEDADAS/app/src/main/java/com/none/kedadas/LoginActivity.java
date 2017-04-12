415c336a22627bcdd71b5eb47bd02adb56a1c0bc
Iñigo Biedma <biedma.64567@e.unavarra.es>
2017-04-12 17:20:17 +0200

    Permisos de lectura del estado del teléfono corregidos.

--- App/KEDADAS/app/src/main/java/com/none/kedadas/LoginActivity.java
+++ App/KEDADAS/app/src/main/java/com/none/kedadas/LoginActivity.java
@@ -24,12 +24,14 @@ public class LoginActivity extends AppCompatActivity {
     public static final String BASE_URL = "http://ec2-52-43-198-218.us-west-2.compute.amazonaws.com/";
     private static final String TAG = LoginActivity.class.getSimpleName();
     private static final int READ_PHONE = 1;
-    private int tryes = 0;
+    private boolean read_phone_allowed = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate()");
         setContentView(R.layout.activity_login);
+        TextView infoTV = (TextView) findViewById(R.id.phoneInfo);
+        infoTV.setText("Es necesario leer el estado del teléfono para la identificación");
     }
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
@@ -38,14 +40,13 @@ public class LoginActivity extends AppCompatActivity {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
+                    read_phone_allowed = true;
+                    TextView infoTV = (TextView) findViewById(R.id.phoneInfo);
+                    infoTV.setText("Permisos configurados correctamente");
 
                 } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
+                    TextView infoTV = (TextView) findViewById(R.id.phoneInfo);
+                    infoTV.setText("Imposible continuar sin permisos de identificación. Si el problema persiste, reinstala la aplicación");
                 }
                 return;
             }
@@ -55,34 +56,55 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
     public void onClick(View view) {
-
-        //Intent intent = new Intent(this, MainPage.class);
-        //startActivity(intent);
-        PhoneInfo phoneData = new PhoneInfo("","");
-        try {
-            phoneData = getPhoneNumber();
-        } catch (SecurityException e) {
-            Toast toast = Toast.makeText(getApplicationContext(), "Error de permisos", Toast.LENGTH_LONG);
-            toast.show();
-            Log.d(TAG, "Security exception",e);
-            // close current activity
+        if (read_phone_allowed) {
+            PhoneInfo phoneData = new PhoneInfo("", "");
+            try {
+                phoneData = getPhoneNumber();
+                //TODO we have to check if we have the pair android id-phone number on ur database with phoneData (moving PhoneInfo class to its own class)
+                loginOrRegister(phoneData.deviceId, phoneData.simId);
+                TextView infoTV = (TextView) findViewById(R.id.phoneInfo);
+                infoTV.setText("deviceId: " + phoneData.deviceId + "\nsimId: " + phoneData.simId);
+                Intent intent = new Intent(this, MainPage.class);
+                startActivity(intent);
+                finish();
+            } catch (SecurityException e) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Error de permisos", Toast.LENGTH_LONG);
+                toast.show();
+                Log.d(TAG, "Security exception", e);
+                // close current activity
+            }
+        } else {
+            requestReadPhonePermission();
         }
-        TextView infoTV = (TextView) findViewById(R.id.phoneInfo);
-        infoTV.setText("deviceId: " + phoneData.deviceId + "\nsimId: " + phoneData.simId);
-        //TODO we have to check if we have the pair android id-phone number on ur database with phoneData (moving PhoneInfo class to its own class)
-        loginOrRegister(phoneData.deviceId,phoneData.simId);
-        Intent intent = new Intent(this, MainPage.class);
-        startActivity(intent);
-        finish();
     }
-
+    private void requestReadPhonePermission() {
+        // TODO: Check different cases (ie first deny then allow)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, READ_PHONE);
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_PHONE_STATE)) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Por favor, reinicia la aplicación", Toast.LENGTH_LONG);
+                toast.show();
+            }else{
+            }
+        }
+    }
     //This method is used to take the phone number from the device automatically
     private PhoneInfo getPhoneNumber() throws SecurityException{
-        // Here, thisActivity is the current activity
+        TelephonyManager mTelephonyManager;
+        mTelephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        Log.d(TAG, "TelephonyManager.getSimSerialNumber = " + mTelephonyManager.getSimSerialNumber());
+        Log.d(TAG, "TelephonyManager.getDeviceId = " + mTelephonyManager.getDeviceId());
+        Log.d(TAG, "TelephonyManager.getLine1Number = " + mTelephonyManager.getLine1Number());
+        Log.d(TAG, "TelephonyManager.getSubscriberId = " + mTelephonyManager.getSubscriberId());
+        return new PhoneInfo(mTelephonyManager.getDeviceId(), mTelephonyManager.getSimSerialNumber());
+        /*
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.READ_PHONE_STATE)
                 != PackageManager.PERMISSION_GRANTED) {
-
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_PHONE_STATE},
+                    READ_PHONE);
             // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.READ_PHONE_STATE)) {
@@ -104,13 +126,7 @@ public class LoginActivity extends AppCompatActivity {
                 // result of the request.
             }
         }
-        TelephonyManager mTelephonyManager;
-        mTelephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        Log.d(TAG, "TelephonyManager.getSimSerialNumber = " + mTelephonyManager.getSimSerialNumber());
-        Log.d(TAG, "TelephonyManager.getDeviceId = " + mTelephonyManager.getDeviceId());
-        Log.d(TAG, "TelephonyManager.getLine1Number = " + mTelephonyManager.getLine1Number());
-        Log.d(TAG, "TelephonyManager.getSubscriberId = " + mTelephonyManager.getSubscriberId());
-        return new PhoneInfo(mTelephonyManager.getDeviceId(), mTelephonyManager.getSimSerialNumber());
+        */
     }
 
     private class PhoneInfo {
