abe48755ab642b7331e2ca403a4482c49843efc4
Iñigo Biedma <biedma.64567@e.unavarra.es>
2017-05-06 19:55:10 +0200

    Corregida tabulación

--- App/KEDADAS/app/src/main/java/com/none/kedadas/FirebaseTest.java
+++ App/KEDADAS/app/src/main/java/com/none/kedadas/FirebaseTest.java
@@ -21,95 +21,95 @@ import com.google.firebase.database.ValueEventListener;
 
 public class FirebaseTest extends AppCompatActivity{
     @Override
-protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_firebase_test);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase_test);
 
-    // Get ListView object from xml
-    final ListView listView = (ListView) findViewById(R.id.listView);
+        // Get ListView object from xml
+        final ListView listView = (ListView) findViewById(R.id.listView);
 
-    // Create a new Adapter
-    final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-            android.R.layout.simple_list_item_1, android.R.id.text1);
+        // Create a new Adapter
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1);
 
-    // Assign adapter to ListView
-    listView.setAdapter(adapter);
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
 
-    // Connect to the Firebase database
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
+        // Connect to the Firebase database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-    // Get a reference to the todoItems child items it the database
-    final DatabaseReference myRef = database.getReference("todoItems");
+        // Get a reference to the todoItems child items it the database
+        final DatabaseReference myRef = database.getReference("todoItems");
 
-    // Assign a listener to detect changes to the child items
-    // of the database reference.
-    myRef.addChildEventListener(new ChildEventListener(){
+        // Assign a listener to detect changes to the child items
+        // of the database reference.
+        myRef.addChildEventListener(new ChildEventListener(){
 
-        // This function is called once for each child that exists
-        // when the listener is added. Then it is called
-        // each time a new child is added.
-        @Override
-        public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
-            String value = dataSnapshot.getValue(String.class);
-            adapter.add(value);
-        }
+            // This function is called once for each child that exists
+            // when the listener is added. Then it is called
+            // each time a new child is added.
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+                String value = dataSnapshot.getValue(String.class);
+                adapter.add(value);
+            }
 
-        // This function is called each time a child item is removed.
-        public void onChildRemoved(DataSnapshot dataSnapshot){
-            String value = dataSnapshot.getValue(String.class);
-            adapter.remove(value);
-        }
+            // This function is called each time a child item is removed.
+            public void onChildRemoved(DataSnapshot dataSnapshot){
+                String value = dataSnapshot.getValue(String.class);
+                adapter.remove(value);
+            }
 
-        // The following functions are also required in ChildEventListener implementations.
-        public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName){}
-        public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName){}
+            // The following functions are also required in ChildEventListener implementations.
+            public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName){}
+            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName){}
 
-        @Override
-        public void onCancelled(DatabaseError error) {
-            // Failed to read value
-            Log.w("TAG:", "Failed to read value.", error.toException());
-        }
-    });
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("TAG:", "Failed to read value.", error.toException());
+            }
+        });
 
-    // Add items via the Button and EditText at the bottom of the window.
-    final EditText text = (EditText) findViewById(R.id.todoText);
-    final Button button = (Button) findViewById(R.id.addButton);
+        // Add items via the Button and EditText at the bottom of the window.
+        final EditText text = (EditText) findViewById(R.id.todoText);
+        final Button button = (Button) findViewById(R.id.addButton);
 
-    button.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
 
-            // Create a new child with a auto-generated ID.
-            DatabaseReference childRef = myRef.push();
+                // Create a new child with a auto-generated ID.
+                DatabaseReference childRef = myRef.push();
 
-            // Set the child's data to the value passed in from the text box.
-            childRef.setValue(text.getText().toString());
+                // Set the child's data to the value passed in from the text box.
+                childRef.setValue(text.getText().toString());
 
-        }
-    });
+            }
+        });
 
-    // Delete items when clicked
-    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        // Delete items when clicked
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-        public void onItemClick(AdapterView<?> parent, View view,
-                                int position, long id) {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
 
-            Query myQuery = myRef.orderByValue().equalTo((String)
-                    listView.getItemAtPosition(position));
+                Query myQuery = myRef.orderByValue().equalTo((String)
+                        listView.getItemAtPosition(position));
 
-            myQuery.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    if (dataSnapshot.hasChildren()) {
-                        DataSnapshot firstChild = dataSnapshot.getChildren().iterator().next();
-                        firstChild.getRef().removeValue();
+                myQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChildren()) {
+                            DataSnapshot firstChild = dataSnapshot.getChildren().iterator().next();
+                            firstChild.getRef().removeValue();
+                        }
                     }
-                }
 
-                @Override
-                public void onCancelled(DatabaseError databaseError) {
-                }
-            })
-            ;}
-    })
-    ;}
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
+            }
+        });
+    }
 }
