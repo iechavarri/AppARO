e3a88399d650f09e9b83f67e1bb10d1faf2e4528
Iñigo Biedma <biedma.64567@e.unavarra.es>
2017-05-07 10:31:14 +0200

    Mostrar las kedadas en el listview con un arrayAdapter

--- App/KEDADAS/app/src/main/java/com/none/kedadas/MasKedadas.java
+++ App/KEDADAS/app/src/main/java/com/none/kedadas/MasKedadas.java
@@ -2,7 +2,17 @@ package com.none.kedadas;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.*;
 /**
  * Created by diego on 23/03/2017.
  */
@@ -14,6 +24,97 @@ public class MasKedadas extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otras_kedadas);
 
+        // Get ListView object from xml
+        final ListView listView = (ListView) findViewById(R.id.ActiveKDD);
+
+        // Create a new Adapter
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1);
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // Connect to the Firebase database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        // Get a reference to the todoItems child items it the database
+        final DatabaseReference myRef = database.getReference("kdds");
+
+
+        // Assign a listener to detect changes to the child items
+        // of the database reference.
+        myRef.addChildEventListener(new ChildEventListener(){
+
+            // This function is called once for each child that exists
+            // when the listener is added. Then it is called
+            // each time a new child is added.
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+                Kedada kdd = dataSnapshot.getValue(Kedada.class);
+                // filter kdd and only add it if it's active
+                adapter.add(kdd.toString());
+            }
+
+            // This function is called each time a child item is removed.
+            public void onChildRemoved(DataSnapshot dataSnapshot) {}/*{
+                Kedada kdd = dataSnapshot.getValue(Kedada.class);
+                adapter.remove(kdd.toString());
+            }
+            */
+            // The following functions are also required in ChildEventListener implementations.
+            public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName){}
+            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName){}
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("TAG:", "Failed to read value.", error.toException());
+            }
+
+        });
+        /*
+        // Add items via the Button and EditText at the bottom of the window.
+        final EditText text = (EditText) findViewById(R.id.todoText);
+        final Button button = (Button) findViewById(R.id.addButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                // Create a new child with a auto-generated ID.
+                DatabaseReference childRef = myRef.push();
+
+                // Set the child's data to the value passed in from the text box.
+                childRef.setValue(text.getText().toString());
+
+            }
+        });
+
+        // Delete items when clicked
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                Query myQuery = myRef.orderByValue().equalTo((String)
+                        listView.getItemAtPosition(position));
+
+                myQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.hasChildren()) {
+                            DataSnapshot firstChild = dataSnapshot.getChildren().iterator().next();
+                            firstChild.getRef().removeValue();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
+            }
+        });
+        */
     }
 
 }
