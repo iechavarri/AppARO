fd4b6e12412d4676c93295e00723ee38184f37be
iechavarri <iechavarribeloqui@gmail.com>
2017-05-14 12:10:36 +0200

    Reconoce links profundos y se abre la app. Cuando se genera el link, se autocopia al portapapeles. Y la fecha se escoge con un datepicker ahora. Falta como agregar info al link, parsearlo y autoañadir usuario a kedada ya creada

--- App/KEDADAS/app/src/main/java/com/none/kedadas/NuevaKedada.java
+++ App/KEDADAS/app/src/main/java/com/none/kedadas/NuevaKedada.java
@@ -1,5 +1,9 @@
 package com.none.kedadas;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,7 +11,10 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -17,19 +24,103 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * Created by diego on 22/03/2017.
  */
 
 public class NuevaKedada extends AppCompatActivity {
+    private TextView tvDisplayDate;
+    private DatePicker dpResult;
+    private Button btnChangeDate;
+
+    private int year;
+    private int month;
+    private int day;
+
+    static final int DATE_DIALOG_ID = 999;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nueva_kedada);
 
+        setCurrentDateOnView();
+        addListenerOnButton();
+
+
     }
+
+    // display current date
+    public void setCurrentDateOnView() {
+
+        tvDisplayDate = (TextView) findViewById(R.id.tvDate);
+
+        final Calendar c = Calendar.getInstance();
+        year = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        day = c.get(Calendar.DAY_OF_MONTH);
+
+        // set current date into textview
+        tvDisplayDate.setText(new StringBuilder()
+                // Month is 0 based, just add 1
+                .append(day).append("/").append(month+1).append("/")
+                .append(year).append(" "));
+
+
+    }
+    public void addListenerOnButton() {
+
+        btnChangeDate = (Button) findViewById(R.id.btnChangeDate);
+
+        btnChangeDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                showDialog(DATE_DIALOG_ID);
+
+            }
+
+        });
+
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_DIALOG_ID:
+                // set date picker as current date
+                return new DatePickerDialog(this, datePickerListener,
+                        year, month,day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener datePickerListener
+            = new DatePickerDialog.OnDateSetListener() {
+
+        // when dialog box is closed, below method will be called.
+        public void onDateSet(DatePicker view, int selectedYear,
+                              int selectedMonth, int selectedDay) {
+            year = selectedYear;
+            month = selectedMonth;
+            day = selectedDay;
+
+            // set selected date into textview
+            tvDisplayDate.setText(new StringBuilder().append(day)
+                    .append("/").append(month + 1).append("/").append(year)
+                    .append(""));
+
+            // set selected date into datepicker also
+            //dpResult.init(year, month, day, null);
+
+        }
+    };
+
+
+
 
     public void crearChat(View view) throws ParseException {
 
@@ -38,11 +129,15 @@ public class NuevaKedada extends AppCompatActivity {
         FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         Kedada.Users userToAdd = new Kedada.Users();
 
+
+
+
         String email = new String();
         String userid = new String();
 
         //Read KDD name
         EditText kddname_edit = (EditText) findViewById(R.id.add_kddname_edittext);
+
         String kddname = kddname_edit.getText().toString();
         if(TextUtils.isEmpty(kddname)) {
             kddname_edit.setError("Debes introducir un nombre de KDD");
@@ -51,12 +146,17 @@ public class NuevaKedada extends AppCompatActivity {
 
         //Read KDD date
         //NO FUNCIONA??
-        EditText kdddate_edit = (EditText) findViewById(R.id.add_kdddate_edittext);
-        String kdddate = kdddate_edit.getText().toString();
+        //EditText kdddate_edit = (EditText) findViewById(R.id.add_kdddate_edittext);
+
+        //String kdddate = kdddate_edit.getText().toString();
+        String kdddate = tvDisplayDate.getText().toString();
+       // String kdddate = day+"/"+month+"/"+year;
         if(TextUtils.isEmpty(kdddate)) {
-            kdddate_edit.setError("Debes introducir una fecha de KDD");
+            //kdddate_edit.setError("Debes introducir una fecha de KDD");
             return;
         }
+
+
 
         //This is for take the actual user data
         FirebaseUser user = firebaseAuth.getCurrentUser();
@@ -77,7 +177,17 @@ public class NuevaKedada extends AppCompatActivity {
         //TODO Creating deeplink for the new kedada
 
         //In this section the new link will be created
-        String url = new String("https://yhj7b.app.goo.gl/"+"?link="+anotherRef.getKey()+"&apn=com.none.kedadas");
+        //https://yhj7b.app.goo.gl/?link=https://yhj7b.app.goo.gl/&apn=com.none.kedadas&afl=http://52.43.198.218
+        //String url = new String("https://yhj7b.app.goo.gl/"+"?link="+anotherRef.getKey()+"&apn=com.none.kedadas");
+        String url = new String("https://yhj7b.app.goo.gl/?link=https://yhj7b.app.goo.gl/&apn=com.none.kedadas&afl=http://52.43.198.218");
+        //This is going to be autocopied to clipboard
+        ClipData clip = ClipData.newPlainText(url,url);
+        ClipboardManager clipboard = (ClipboardManager)this.getSystemService(CLIPBOARD_SERVICE);
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(getApplicationContext(), "URL copiada al portapapeles", Toast.LENGTH_LONG).show();
+
+
+
         //"https://domain/?link=your_deep_link&apn=package_name[&amv=minimum_version][&ad=1][&al=android_link][&afl=fallback_link]"
         Log.d("LINKGORRINO",url);
 
@@ -94,4 +204,8 @@ public class NuevaKedada extends AppCompatActivity {
         startActivity(intent);
         */
     }
+
 }
+
+
+
